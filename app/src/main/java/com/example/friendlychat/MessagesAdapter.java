39d7138005f9d595221e65504edd70c4519326dc
Ali Hassan <alihassan17122002@gmail.com>
2021-07-25 07:41:09 -0800

    Created MessagesAdapter to display messages.

---
+++ app/src/main/java/com/example/friendlychat/MessagesAdapter.java
@@ -0,0 +1,41 @@
+package com.example.friendlychat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessageViewHolder> {
+
+    private Context mContext;
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(mContext).inflate(R.layout.message_view_holder, parent, false);
+        return new MessageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+
+        /*will be done in the next commit*/
+    }
+
+    @Override
+    public int getItemCount() {
+        /*will be done in the next commit*/
+        return 0;
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder{
+        /*will be done in the next commit*/
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
