023139de9cfce9c8ad1dba092d764412408db03a
Ali Hassan <alihassan17122002@gmail.com>
2021-08-03 22:57:56 -0800

    increased the ChatsAdapter binding performance.

--- app/src/main/java/com/example/friendlychat/Adapters/MessagesAdapter.java
+++ app/src/main/java/com/example/friendlychat/Adapters/MessagesAdapter.java
@@ -1,6 +1,7 @@
 package com.example.friendlychat.Adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,11 +22,12 @@ import java.util.List;
 
 public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessageViewHolder> {
 
+    private static final String TAG = MessagesAdapter.class.getSimpleName();
     private Context mContext;
     private List<Message> messages;
 
-    private static final int USE_NORMAL_BACKGROUND = 0;
-    private static final int USE_CURRENT_MESSAGE_BACKGROUND = 1;
+    private static final int USE_SENDER_BACKGROUND = 1;
+    private static final int USE_LOCAL_BACKGROUND = 0;
     public void setMessages(List<Message> messages) {
 
         this.messages = messages;
@@ -38,12 +40,29 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.Messag
         this.messages = messages;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        String userName = messages.get(position).getName();
+        if (useCurrentMessageBackground(userName)){
+            return USE_LOCAL_BACKGROUND;
+        }else {
+            return USE_SENDER_BACKGROUND;
+        }
+    }
+
     @NonNull
     @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(mContext).inflate(R.layout.message_view_holder, parent, false);
-        return new MessageViewHolder(v);
+        Log.d(TAG, "onCreateViewHolder");
+        if (viewType == USE_LOCAL_BACKGROUND){
+            View v = LayoutInflater.from(mContext).inflate(R.layout.local_message_view_holder, parent, false);
+            return new MessageViewHolder(v);
+        }else {
+            View v2 = LayoutInflater.from(mContext).inflate(R.layout.message_view_holder, parent, false);
+            return new MessageViewHolder(v2);
+        }
+
     }
 
 
@@ -74,9 +93,9 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.Messag
             }
             holder.authorName.setText(name);
             if (useCurrentMessageBackground(name)){
+
                 holder.messageLayout.setBackground(mContext.getResources().getDrawable(R.drawable.current_message_background));
                 /*paramsMsg.gravity = Gravity.END;*/
-                holder.layout.setGravity(Gravity.END);
             }
         }
         /*will be done in the next commit*/
@@ -101,7 +120,6 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.Messag
             messageTextView = itemView.findViewById(R.id.messageTextView);
             imageView = itemView.findViewById(R.id.photoImageView);
             authorName = itemView.findViewById(R.id.nameTextView);
-            layout = itemView.findViewById(R.id.bodyView);
             messageLayout = itemView.findViewById(R.id.messageLayout);
         }
 
