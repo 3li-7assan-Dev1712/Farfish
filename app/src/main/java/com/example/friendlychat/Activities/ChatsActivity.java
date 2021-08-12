05e8cb8a16de88c9649f56bf7f2a852b36c3b354
Ali Hassan <alihassan17122002@gmail.com>
2021-08-11 17:36:57 -0800

    fixed a bug in ChatsActivity when it is onPause() and the user was writing a message then should save the state of the user that they are not writing (they are just Online).

--- app/src/main/java/com/example/friendlychat/Activities/ChatsActivity.java
+++ app/src/main/java/com/example/friendlychat/Activities/ChatsActivity.java
@@ -43,9 +43,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.MetadataChanges;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -338,6 +336,7 @@ public class ChatsActivity extends AppCompatActivity {
         }
         else if (isActive) {
             chat_last_seen.setText(getResources().getString(R.string.online));
+            chat_last_seen.setTextColor(getResources().getColor(R.color.colorTitle));
         }
         else {
 
@@ -504,5 +503,12 @@ public class ChatsActivity extends AppCompatActivity {
         mMessageRecyclerView.smoothScrollToPosition(messages.size() - 1);
     }
 
+    /*when the user navigate out from the activity (closing the app or navigate to other chat) while is writing. The server
+    * should be notified*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setUserIsNotWriting();
 
+    }
 }
