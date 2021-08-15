93264aa190ab025efdd552e6fa7db9a0a3b81a25
Ali Hassan <alihassan17122002@gmail.com>
2021-08-14 23:08:30 -0800

    design the sign in Activity for a custom sign in instead of firebase UI.

---
+++ app/src/main/java/com/example/friendlychat/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.friendlychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
