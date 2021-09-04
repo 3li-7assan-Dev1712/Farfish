be486c06a6c907ff2cb12b823e709e75f9d4e2f9
Ali Hassan <alihassan17122002@gmail.com>
2021-09-04 11:08:24 -0800

    Created the user fragment class and file and linked them together.

---
+++ app/src/main/java/com/example/friendlychat/fragments/UsersFragment.java
@@ -0,0 +1,27 @@
+package com.example.friendlychat.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.friendlychat.R;
+
+public class UsersFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         return inflater.inflate(R.layout.users_fragment, container, false);
+    }
+}
