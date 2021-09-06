52b5352d59d8729b9039392c4df4dfe03d73382d
Ali Hassan <alihassan17122002@gmail.com>
2021-09-06 10:19:11 -0800

    created a new fragment to display full images from chats fragment.

---
+++ app/src/main/java/com/example/friendlychat/fragments/FullImageFragment.java
@@ -0,0 +1,25 @@
+package com.example.friendlychat.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.friendlychat.R;
+
+public class FullImageFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.full_image_fragment, container, false);
+    }
+}
