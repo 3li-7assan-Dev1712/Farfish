ec1cafa804698f6045acf432af81f105c41b471b
Ali Hassan <alihassan17122002@gmail.com>
2021-09-08 07:44:29 -0800

    attach the status fragment with navigation component and navigate to it through the bottom navigation view.

--- app/src/main/java/com/example/friendlychat/fragments/StatusFragment.java
+++ app/src/main/java/com/example/friendlychat/fragments/StatusFragment.java
@@ -4,19 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.friendlychat.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StatusFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.status_fragment, container, false);
+        View view = inflater.inflate(R.layout.status_fragment, container, false);
+        FloatingActionButton uploadImageFab = view.findViewById(R.id.uploadImageStatusFab);
+        uploadImageFab.setOnClickListener( uploadImage -> {
+            Toast.makeText(requireContext(), "Upload image as a status", Toast.LENGTH_SHORT).show();
+        });
+        FloatingActionButton uploadTextFab = view.findViewById(R.id.uploadTextStatusFab);
+        uploadTextFab.setOnClickListener( uploadImage -> {
+            Toast.makeText(requireContext(), "Upload text as a status", Toast.LENGTH_SHORT).show();
+        });
+        return view;
     }
 
     @Override
