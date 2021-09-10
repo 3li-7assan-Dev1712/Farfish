da8335a42e451009b5c9b5868268fae9f19c90f6
Ali Hassan <alihassan17122002@gmail.com>
2021-09-09 16:42:02 -0800

    receive the data and use it with toolbar.

--- app/src/main/java/com/example/friendlychat/fragments/StatusDetailFragment.java
+++ app/src/main/java/com/example/friendlychat/fragments/StatusDetailFragment.java
@@ -13,13 +13,23 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.friendlychat.Module.Status;
 import com.example.friendlychat.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class StatusDetailFragment extends Fragment {
 
+    private List<Status> userStatues;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* get the argument from the parent fragment*/
+        Status[] statuses = (Status[]) getArguments().getParcelableArray("one_user_statuses");
+        userStatues = Arrays.asList(statuses);
     }
 
     @Nullable
@@ -27,6 +37,10 @@ public class StatusDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View statusFragment = inflater.inflate(R.layout.status_detail_fragment, container, false);
         Toolbar statusBar = statusFragment.findViewById(R.id.toolbar_status_detail);
+        statusBar.setTitle(userStatues.get(0).getUploaderName());
+        SimpleDateFormat d = new SimpleDateFormat("h:mm a", Locale.getDefault());
+        String readableDate = d.format(userStatues.get(0).getTimestamp());
+        statusBar.setSubtitle(readableDate);
         statusBar.setNavigationOnClickListener( navigationIconView -> {
             Navigation.findNavController(statusFragment).navigateUp();
         });
