5fb119d74ebafa819be7c5740df7ae49bcd91e20
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 07:44:45 -0800

    built the splash screen and used it

--- app/src/main/java/com/example/friendlychat/Activities/SplashScreen.java
+++ app/src/main/java/com/example/friendlychat/Activities/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.friendlychat.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.friendlychat.R;
 
@@ -11,6 +13,9 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        new Handler().postDelayed( () -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }, 3000);
     }
 }
