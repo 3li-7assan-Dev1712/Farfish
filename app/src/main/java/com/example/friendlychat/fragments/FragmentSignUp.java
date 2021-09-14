692420c50754cb462be799067f4e6398c953a33a
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 06:55:21 -0800

    update the text color for SignIn layout

--- app/src/main/java/com/example/friendlychat/fragments/FragmentSignUp.java
+++ app/src/main/java/com/example/friendlychat/fragments/FragmentSignUp.java
@@ -22,6 +22,7 @@ public class FragmentSignUp extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
