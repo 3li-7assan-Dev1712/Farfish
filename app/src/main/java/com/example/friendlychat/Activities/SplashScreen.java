74913584c154cb020bba1a6841e6c66e793a7732
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 09:17:22 -0800

    update the splash screen duration

--- app/src/main/java/com/example/friendlychat/Activities/SplashScreen.java
+++ app/src/main/java/com/example/friendlychat/Activities/SplashScreen.java
@@ -16,6 +16,6 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed( () -> {
             startActivity(new Intent(SplashScreen.this, MainActivity.class));
             finish();
-        }, 3000);
+        }, 1500);
     }
 }
