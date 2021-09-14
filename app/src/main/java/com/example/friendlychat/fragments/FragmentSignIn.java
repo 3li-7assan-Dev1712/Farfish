0d1e25f96b04f31404a11082b980b4c5a7c94f22
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 13:32:19 -0800

    used the new customized sign in fragment to let users sign in.

--- app/src/main/java/com/example/friendlychat/fragments/FragmentSignIn.java
+++ app/src/main/java/com/example/friendlychat/fragments/FragmentSignIn.java
@@ -1,6 +1,7 @@
 package com.example.friendlychat.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +15,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.friendlychat.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FragmentSignIn extends Fragment {
+    private static final String TAG = FragmentSignIn.class.getSimpleName();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         requireActivity().findViewById(R.id.bottom_nav).setVisibility(View.GONE);
@@ -34,13 +41,16 @@ public class FragmentSignIn extends Fragment {
         });
         Button loginButton = view.findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener( loginButtonListener -> {
-            if (emailSignInTextView.getText().toString().equals(""))
+            String email = emailSignInTextView.getText().toString();
+            String password = passwordSignInTextView.getText().toString();
+            if (email.equals(""))
                 displayRequiredFieldToast("Please enter you e-mail address to sign in");
-            else if (passwordSignInTextView.getText().toString().equals(""))
+            else if (password.equals(""))
                 displayRequiredFieldToast("Please enter you password to sign in");
             else{
                 // sign in functionality
                 Toast.makeText(requireContext(), "You are ready to sign in", Toast.LENGTH_SHORT).show();
+                signIn(email, password);
             }
         });
         TextView register = view.findViewById(R.id.register_sign_in);
@@ -48,6 +58,28 @@ public class FragmentSignIn extends Fragment {
             Navigation.findNavController(view).navigate(FragmentSignInDirections.actionFragmentSignInToFragmentSignUp());
         });
         return view;
+    }
+
+    private void signIn(String email, String password) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+       /* auth.signOut();*/
+        AuthCredential credential = EmailAuthProvider.getCredential(email, password);
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(requireActivity(), "successfully sin in", Toast.LENGTH_SHORT).show();
+
+                Log.d(TAG, "signIn: sign in successfully " + auth.getUid());
+            }else if (task.getException() != null){
+                auth.signInWithCredential(credential).addOnCompleteListener( comleteTask ->{
+                    String id = comleteTask.getResult().getUser().getIdToken(true).toString();
+                    Log.d(TAG, "signIn: id : " + id);
+                }).addOnFailureListener(exc -> {
+                    Log.d(TAG, "signIn: exception me, esage : " + exc.getMessage());
+                });
+            }
+        }).addOnFailureListener(e -> {
+            Log.d(TAG, "signIn: " +e.getMessage());
+        });
     }
 
     private void displayRequiredFieldToast(String message) {
