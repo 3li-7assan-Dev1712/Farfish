7792786ffd0f8db16edd2d01efbf0394be00e330
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 10:02:46 -0800

    check for required field in FragmentSignIn

--- app/src/main/java/com/example/friendlychat/fragments/FragmentSignIn.java
+++ app/src/main/java/com/example/friendlychat/fragments/FragmentSignIn.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,10 +26,30 @@ public class FragmentSignIn extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        TextView emailSignInTextView = view.findViewById(R.id.editTextEmailSignIn);
+        TextView passwordSignInTextView = view.findViewById(R.id.editTextPasswordSignIn);
+        TextView forgotPassWord = view.findViewById(R.id.forgotPasswordSignIn);
+        forgotPassWord.setOnClickListener( forgotPassWordListener -> {
+            // forgot password functionality
+        });
+        Button loginButton = view.findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener( loginButtonListener -> {
+            if (emailSignInTextView.getText().toString().equals(""))
+                displayRequiredFieldToast("Please enter you e-mail address to sign in");
+            else if (passwordSignInTextView.getText().toString().equals(""))
+                displayRequiredFieldToast("Please enter you password to sign in");
+            else{
+                // sign in functionality
+            }
+        });
         TextView register = view.findViewById(R.id.register_sign_in);
         register.setOnClickListener( registerTextView -> {
             Navigation.findNavController(view).navigate(FragmentSignInDirections.actionFragmentSignInToFragmentSignUp());
         });
         return view;
+    }
+
+    private void displayRequiredFieldToast(String message) {
+        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show();
     }
 }
