aba5157efeb4a7517c5c6c6f79e44277393a6c2a
Ali Hassan <alihassan17122002@gmail.com>
2021-09-14 07:30:44 -0800

    start creating a splash screen for the app

---
+++ app/src/main/java/com/example/friendlychat/Activities/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.friendlychat.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.friendlychat.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
