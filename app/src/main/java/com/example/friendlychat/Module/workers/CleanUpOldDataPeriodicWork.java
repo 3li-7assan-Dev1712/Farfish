65e85b7c9a46374f679e50a70e6937b44ad4989f
Ali Hassan <alihassan17122002@gmail.com>
2021-09-25 09:25:06 -0800

    implemented the second periodic work for deleting outdated messages.

--- app/src/main/java/com/example/friendlychat/Module/workers/CleanUpOldDataPeriodicWork.java
+++ app/src/main/java/com/example/friendlychat/Module/workers/CleanUpOldDataPeriodicWork.java
@@ -7,22 +7,29 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.friendlychat.Module.Message;
 import com.example.friendlychat.Module.MessagesPreference;
 import com.example.friendlychat.Module.Status;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CleanUpOldDataPeriodicWork extends Worker {
 
     // tag for logging and debugging
     private static final String TAG = CleanUpOldDataPeriodicWork.class.getSimpleName();
+    // three months
+    private static final long MAX_MESSAGE_AGE = 90;
     // status reference where in the clean up happens
     private DatabaseReference mDatabaseStatusReference = FirebaseDatabase.getInstance().getReference("status");
+    private DatabaseReference mDatabaseMessageReference = FirebaseDatabase.getInstance().getReference("rooms");
     // firestore references from stories and messages with images
     private StorageReference mOutdatedStoryImageReference = FirebaseStorage.getInstance().getReference("stories");
     private StorageReference mOutdatedMessageImageReference = FirebaseStorage.getInstance().getReference("images");
@@ -40,6 +47,7 @@ public class CleanUpOldDataPeriodicWork extends Worker {
     public Result doWork() {
 
         String currentUserId = MessagesPreference.getUserId(getApplicationContext());
+        long currentTimeInMillis = System.currentTimeMillis();
 
         // clean up old statuses
         DatabaseReference userStatusReference = mDatabaseStatusReference.child(currentUserId);
@@ -48,7 +56,7 @@ public class CleanUpOldDataPeriodicWork extends Worker {
             Iterable<DataSnapshot> statusesIterable = snapshot.getChildren();
             for (DataSnapshot status : statusesIterable) {
                 Status statusToCheck = status.getValue(Status.class);
-                long currentTimeInDays = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis());
+                long currentTimeInDays = TimeUnit.MILLISECONDS.toDays(currentTimeInMillis);
                 assert statusToCheck != null;
                 long statusTimeInDays = TimeUnit.MILLISECONDS.toDays(statusToCheck.getTimestamp());
                 long statusAge = currentTimeInDays - statusTimeInDays;
@@ -62,7 +70,40 @@ public class CleanUpOldDataPeriodicWork extends Worker {
             }
         });
 
-        // clean up old messages will be in the next commit *_*
+        // clean up old messages
+      /*  List<Message> newMessages = new ArrayList<>();*/
+        DatabaseReference userMessageRooms = mDatabaseMessageReference.child(currentUserId);
+        userMessageRooms.get().addOnSuccessListener(snapshot -> {
+
+            Iterable<DataSnapshot> userRooms = snapshot.getChildren();
+            for (DataSnapshot singleRoom : userRooms) {
+                Iterable<DataSnapshot> messagesIterable = singleRoom.getChildren();
+              /*  Message newMessage = null;*/
+                for (DataSnapshot messageSnapshot : messagesIterable) {
+
+                    Message outdatedMessage = messageSnapshot.getValue(Message.class);
+                    // after get the message let's see if it's outdated or not
+                    assert outdatedMessage != null;
+                    long messageTimeInDays = TimeUnit.MILLISECONDS.toDays(outdatedMessage.getTimestamp());
+                    long currentTimeInDays = TimeUnit.MILLISECONDS.toDays(currentTimeInMillis);
+                    long messageAgeInDays = currentTimeInDays - messageTimeInDays;
+                    if (messageAgeInDays >= MAX_MESSAGE_AGE) {
+                        // the message is outdated and should be deleted
+                        String outdatedMessagePhotoUrl = outdatedMessage.getPhotoUrl();
+                        if (!outdatedMessagePhotoUrl.equals(""))
+                            removeImageFromFirestore(outdatedMessagePhotoUrl, 2);
+                        messageSnapshot.getRef().removeValue().addOnSuccessListener(successfullRemove -> {
+                            Log.d(TAG, "doWork: remove outdated message successfully");
+                        });
+                    }else {
+                      /*  newMessage = outdatedMessage;*/
+                    }
+                }
+               /* if (newMessage != null)
+                    newMessages.add(newMessage);*/
+            }
+        });
+
 
         return Result.success();
     }
